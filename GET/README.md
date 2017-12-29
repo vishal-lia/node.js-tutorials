0ec206ebc75b3ce7441c63a2f95aa79063550df8
vishal-lia <vishallia77@gmail.com>
2017-12-29 16:41:23 +0530

    HTTP GET using node.js

---
+++ GET/README.md
@@ -0,0 +1,2 @@
+#HTTP-GET
+This tutorial is about how to make a HTTP GET request to validate a form using node.js
---
+++ GET/httpServer.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var module = require('./dbModule');
+
+var server = http.createServer(function(request, response) {
+    var query = url.parse(request.url).query;
+    var username = querystring.parse(query)['username'];
+    var password = querystring.parse(query)['password'];
+
+    var result = module.authenticateUser(username, password);
+    
+    response.writeHead(200,{"ContentType": "text/html"});
+    response.write("<html><body><center><h3>" + result +"</h3></center></body></html>");
+    response.end();
+});
+
+server.listen(3000);
+console.log("Server started, listen to port 3000");
