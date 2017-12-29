3a3c6b7a47898f6d5ab077f60ce78256960ec144
vishal-lia <vishallia77@gmail.com>
2017-12-29 16:46:35 +0530

    HTTP POST example

---
+++ POST/README.md
@@ -0,0 +1,2 @@
+# HTTP-GET 
+This tutorial is about how to make a HTTP POST request to validate a form using node.js
---
+++ POST/dbModule.js
@@ -0,0 +1,8 @@
+exports.authenticateUser = function(username, password) {
+    if(username === "admin" && password === "admin") {
+        return "Valid User";
+    }
+    else {
+        return "Invalid User";
+    }
+}
