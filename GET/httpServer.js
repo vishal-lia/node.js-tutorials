a65fd5fcbd4211ac3aee7b4009fcadf784214ecb
vishal-lia <33822137+vishal-lia@users.noreply.github.com>
2018-01-01 09:01:39 +0530

    Update httpServer.js

--- GET/httpServer.js
+++ GET/httpServer.js
@@ -16,4 +16,4 @@ var server = http.createServer(function(request, response) {
 });
 
 server.listen(3000);
-console.log("Server started, listen to port 3000");
+console.log("Server started, listening on port 3000");
