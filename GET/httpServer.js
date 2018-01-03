69b0f85892757ce30d6838945b9ec2a8f64e6e3f
vishal-lia <33822137+vishal-lia@users.noreply.github.com>
2018-01-03 16:02:48 +0530

    Update httpServer.js

--- GET/httpServer.js
+++ GET/httpServer.js
@@ -10,7 +10,7 @@ var server = http.createServer(function(request, response) {
 
     var result = module.authenticateUser(username, password);
     
-    response.writeHead(200,{"ContentType": "text/html"});
+    response.writeHead(200,{"Content-Type": "text/html"});
     response.write("<html><body><center><h3>" + result +"</h3></center></body></html>");
     response.end();
 });
